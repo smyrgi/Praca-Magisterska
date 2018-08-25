2b27d3c873627bab674c03ddefa338d468840e4f
Kamila <kama_smyrek94@o2.pl>
2018-08-25 13:40:58 +0200

    package file

---
+++ package_script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import dane
+import odstajace
+import review_manually_fixed
+import find_matching
+
+dane.main()
+odstajace.main()
+review_manually_fixed.main()
+find_matching.main()
+
