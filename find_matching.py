32ea1c0309c74b6abfa03ac43ce006ba531f65ac
Kamila <kama_smyrek94@o2.pl>
2018-09-27 22:23:55 +0200

    Update find_matching.py
    changed way of matching groups

--- find_matching.py
+++ find_matching.py
@@ -11,29 +11,27 @@ def main():
 	output_file_name_groups = 'matched_groups.txt'
 	output_file_name_data = 'matched_groups_data.txt'
 	
-	columns_names = ['Year', 'CarringtonRotation', 'NOAA','StartJulianDay','EndJulianDay','TotalGroupArea',
-						   'WeightedLongitude', 'WeightedLatitude','StartPosition','EndPosition','MinLongitude',
-						   'MaxLongitude','LongitudeRange', 'MinLatitude','MaxLatitude','LatitudeRange']
-
 	sunspot_data_fixedlimits = pd.read_table(input_file_name_fixedlimits, header=0)
 	sunspot_data_manually = pd.read_table(input_file_name_manually, header=0)
-	
+		
 	sunspot_data = sunspot_data_fixedlimits
 	sunspot_data = sunspot_data.append(sunspot_data_manually, ignore_index=True)
+	sunspot_data = sunspot_data.sort_values(by='CenterJulianDay')
 	
-	data_on_east = sunspot_data.loc[sunspot_data['StartPosition'] < 0.1]
-	data_on_west = sunspot_data.loc[sunspot_data['EndPosition'] > 0.9]
+	#data_on_east = sunspot_data.loc[sunspot_data['StartPosition'] < 0.1]
+	data_not_disappearing = sunspot_data.loc[sunspot_data['EndPosition'] > 0.8]
 	
-	data_on_east = data_on_east.sort_values(by='StartJulianDay')
-	data_on_west = data_on_west.sort_values(by='EndJulianDay')
-	data_on_west['ExpectedStartJulianDay'] = data_on_west['EndJulianDay'] + get_half_rotation_period(data_on_west['WeightedLatitude'])
+	#data_on_east = data_on_east.sort_values(by='StartJulianDay')
+	data_not_disappearing = data_not_disappearing.sort_values(by='CenterJulianDay')
+	data_not_disappearing['ExpectedCenterJulianDay'] = data_not_disappearing['CenterJulianDay'] + get_rotation_period(data_not_disappearing['WeightedLatitude'])
 	
-	groups_matched = get_matched_groups(data_on_west, data_on_east)
+	groups_matched = get_matched_groups(data_not_disappearing, sunspot_data)
 
 	with open(output_file_name_groups, 'w') as ofile:
 		for pair in groups_matched:
 			ofile.write('\t'.join(pair)+'\n')
 		
+	columns_names = sunspot_data.columns
 	groups_matched_all = [group for pair in groups_matched for group in pair]
 	data_groups_matched = sunspot_data.loc[sunspot_data['NOAA'].isin(groups_matched_all)]
 	data_groups_matched = data_groups_matched.sort_values(by='NOAA')
@@ -41,34 +39,46 @@ def main():
 	
 		
 		
-def get_half_rotation_period(latitude_value):
+def get_rotation_period(latitude_value):
 	dt = 1 * u.day
 	latitude = u.Quantity(latitude_value, 'deg')
 	rotation_rate = diff_rot(dt, latitude) / dt 
-	rotation_period = 180 * u.deg / rotation_rate
+	rotation_period = 360 * u.deg / rotation_rate
 	return rotation_period.value
 	
 	
-def get_matched_groups(data_on_west, data_on_east):
+def get_matched_groups(data_not_disappearing, sunspot_data): #groups_matched.append([group_data_not_disappearing['NOAA'], group_data_on_east['NOAA']])
 	groups_matched = []
-	for index_west, group_data_on_west in data_on_west.iterrows():
-		for index_east, group_data_on_east in data_on_east.iterrows():
-			if (approximately_equal(group_data_on_west['ExpectedStartJulianDay'], group_data_on_east['StartJulianDay'])):
+	for index_west, group_data_not_disappearing in data_not_disappearing.iterrows():
+		for index_east, group_data in sunspot_data.iterrows():
+			if (check_same_day(group_data_not_disappearing['ExpectedCenterJulianDay'], group_data['CenterJulianDay'])):
 				# times matched
-				if (approximately_equal(group_data_on_west['WeightedLatitude'], group_data_on_east['WeightedLatitude'], precision = 0.5)):
+				if (check_close_position(group_data_not_disappearing, group_data)):
 					# latitude matched
-					groups_matched.append([group_data_on_west['NOAA'], group_data_on_east['NOAA']])
-				break
-			elif (group_data_on_west['ExpectedStartJulianDay'] < group_data_on_east['StartJulianDay']):
+					groups_matched.append([group_data_not_disappearing['NOAA'], group_data['NOAA']])
+					break
+			elif (group_data_not_disappearing['ExpectedCenterJulianDay'] < group_data['CenterJulianDay']):
 				# times not matched
 				break
+		
 	return groups_matched
 	
 	
-def approximately_equal(value1, value2, precision = 1):
-	if (abs(value1 - value2) < precision):
+def check_same_day(value1, value2):
+	if (abs(value1 - value2) < 1): #1 dzień
 		return True
 	return False
+		
+	
+def check_close_position(group_data1, group_data2):
+	precision_longitude = 10
+	precision_latitude = 1
+		
+	if ((abs(group_data1['WeightedLongitude'] - group_data2['WeightedLongitude']) < precision_longitude) and
+		(abs(group_data1['WeightedLatitude'] - group_data2['WeightedLatitude']) < precision_latitude)):
+		return True
+	return False
+
 		
 	
 main()
