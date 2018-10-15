d6b42bc053bbe7be2d90c7de081288576c48cdc7
Kamila <kama_smyrek94@o2.pl>
2018-10-15 22:16:29 +0200

    Update find_matching.py
    don't output matched groups' data
    remove commented code
    julian days as floats not integers
    one value for rotation (27.2753)
    check matched groups in rectangles

--- find_matching.py
+++ find_matching.py
@@ -9,7 +9,6 @@ def main():
 	input_file_name_fixedlimits = 'groupdata_fixedlimits.txt'
 	input_file_name_manually = 'groupdata_modified_manually.txt'
 	output_file_name_groups = 'matched_groups.txt'
-	output_file_name_data = 'matched_groups_data.txt'
 	
 	sunspot_data_fixedlimits = pd.read_table(input_file_name_fixedlimits, header=0)
 	sunspot_data_manually = pd.read_table(input_file_name_manually, header=0)
@@ -18,56 +17,55 @@ def main():
 	sunspot_data = sunspot_data.append(sunspot_data_manually, ignore_index=True)
 	sunspot_data = sunspot_data.sort_values(by='CenterJulianDay')
 	
-	#data_on_east = sunspot_data.loc[sunspot_data['StartPosition'] < 0.1]
 	data_not_disappearing = sunspot_data.loc[sunspot_data['EndPosition'] > 0.8]
-	
-	#data_on_east = data_on_east.sort_values(by='StartJulianDay')
 	data_not_disappearing = data_not_disappearing.sort_values(by='CenterJulianDay')
-	data_not_disappearing['ExpectedCenterJulianDay'] = data_not_disappearing['CenterJulianDay'] + get_rotation_period(data_not_disappearing['WeightedLatitude'])
-	data_not_disappearing['CenterJulianDay'] = data_not_disappearing['CenterJulianDay'].astype(int)
-	data_not_disappearing['ExpectedCenterJulianDay'] = data_not_disappearing['ExpectedCenterJulianDay'].astype(int)
+	data_not_disappearing['ExpectedCenterJulianDay'] = data_not_disappearing['CenterJulianDay'] + 27.2753
+	data_not_disappearing['CenterJulianDay'] = data_not_disappearing['CenterJulianDay']
+	data_not_disappearing['ExpectedCenterJulianDay'] = data_not_disappearing['ExpectedCenterJulianDay']
 	
 	groups_matched = get_matched_groups(data_not_disappearing, sunspot_data)
 
 	with open(output_file_name_groups, 'w') as ofile:
-		for pair in groups_matched:
-			ofile.write('\t'.join(pair)+'\n')
-		
-	columns_names = sunspot_data.columns
-	groups_matched_all = [group for pair in groups_matched for group in pair]
-	data_groups_matched = sunspot_data.loc[sunspot_data['NOAA'].isin(groups_matched_all)]
-	data_groups_matched = data_groups_matched.sort_values(by='NOAA')
-	data_groups_matched.to_csv( output_file_name_data, sep='	', header = columns_names, index = None )
+		for match in groups_matched:
+			ofile.write('\t'.join(match)+'\n')
+					
 	
-		
-		
-def get_rotation_period(latitude_value):
-	dt = 1 * u.day
-	latitude = u.Quantity(latitude_value, 'deg')
-	rotation_rate = diff_rot(dt, latitude) / dt 
-	rotation_period = 360 * u.deg / rotation_rate
-	return rotation_period.value
-	
-	
-def get_matched_groups(data_not_disappearing, sunspot_data): #groups_matched.append([group_data_not_disappearing['NOAA'], group_data_on_east['NOAA']])
+def get_matched_groups(data_not_disappearing, sunspot_data): 
 	groups_matched = []
 	for index_west, group_data_not_disappearing in data_not_disappearing.iterrows():
 		data_recurrent = sunspot_data.loc[abs(sunspot_data['CenterJulianDay'] - group_data_not_disappearing['ExpectedCenterJulianDay']) < 1]
 		# times matched
 		for index_east, group_data in data_recurrent.iterrows():
-			if (check_close_position(group_data_not_disappearing, group_data)):
-				# latitude matched
-				groups_matched.append([group_data_not_disappearing['NOAA'], group_data['NOAA']])
-				break
+			precision_longitude = 5
+			precision_latitude = 1
+			if (check_close_position(group_data_not_disappearing, group_data, precision_longitude, precision_latitude)):
+				# position matched
+				groups_matched.append([str(precision_longitude), str(group_data_not_disappearing['Year']), 
+									   str(group_data_not_disappearing['CarringtonRotation']), group_data_not_disappearing['NOAA'], 
+									   group_data['NOAA']])
+			else:
+				precision_longitude = 10
+				precision_latitude = 2
+				if (check_close_position(group_data_not_disappearing, group_data, precision_longitude, precision_latitude)):
+					# position matched
+					groups_matched.append([str(precision_longitude), str(group_data_not_disappearing['Year']), 
+										   str(group_data_not_disappearing['CarringtonRotation']), group_data_not_disappearing['NOAA'], 
+										   group_data['NOAA']])
+				else:
+					precision_longitude = 15
+					precision_latitude = 3
+					if (check_close_position(group_data_not_disappearing, group_data, precision_longitude, precision_latitude)):
+						# position matched
+						groups_matched.append([str(precision_longitude), str(group_data_not_disappearing['Year']), 
+											   str(group_data_not_disappearing['CarringtonRotation']), group_data_not_disappearing['NOAA'], 
+											   group_data['NOAA']])
+				
 	return groups_matched
 
 	
-def check_close_position(group_data1, group_data2):
-	precision_longitude = 10
-	precision_latitude = 1
-		
-	if ((abs(group_data1['WeightedLongitude'] - group_data2['WeightedLongitude']) < precision_longitude) and
-		(abs(group_data1['WeightedLatitude'] - group_data2['WeightedLatitude']) < precision_latitude)):
+def check_close_position(group_data1, group_data2, precision_longitude, precision_latitude):
+	if ((abs(group_data1['WeightedLongitude'] - group_data2['WeightedLongitude']) <= precision_longitude) and
+		(abs(group_data1['WeightedLatitude'] - group_data2['WeightedLatitude']) <= precision_latitude)):
 		return True
 	return False
 
