89b06841e85841c35a823fa82a5dc5f804343e92
Kamila <kama_smyrek94@o2.pl>
2018-10-16 20:36:10 +0200

    new modul perform added

--- package_script.py
+++ package_script.py
@@ -4,9 +4,11 @@ import prepare_data
 import check_outliers
 import review_manually_fixed
 import find_matching
+import output_matched_data
 
 prepare_data.main()
 check_outliers.main()
 review_manually_fixed.main()
 find_matching.main()
+output_matched_data.main()
 
