4c9682eea1d99ec3476b851a34d969eb23086062
Kamila <kama_smyrek94@o2.pl>
2018-11-08 21:26:37 +0100

    input file columns changed

--- find_matching.py
+++ find_matching.py
@@ -14,11 +14,13 @@ def main():
 	
 	
 def find_matching(input_file_name, output_file_name):
-	sunspot_data = pd.read_fwf(input_file_name, header=0)
-	sunspot_data = sunspot_data.sort_values(by='StartJulianDay')
+	columns_positions = [(0, 4), (5, 12), (13, 20), (21, 29), (30, 38), (39, 45), (46, 55), (56, 64), (65, 73), (74, 80), (81, 88), (89, 96), (97, 106), (107, 113), (114, 120), (121, 129)]
+
+	sunspot_data = pd.read_fwf(input_file_name, header=0, colspecs=columns_positions)
+	sunspot_data = sunspot_data.sort_values(by='StartDay')
 	
-	data_not_disappearing = sunspot_data.loc[sunspot_data['EndPosition'] > 0.8]
-	data_not_disappearing = data_not_disappearing.sort_values(by='StartJulianDay')
+	data_not_disappearing = sunspot_data.loc[sunspot_data['EndPos'] > 0.8]
+	data_not_disappearing = data_not_disappearing.sort_values(by='StartDay')
 	
 	groups_matched = get_matched_groups(data_not_disappearing, sunspot_data)
 	
@@ -34,19 +36,19 @@ def get_matched_groups(data_not_disappearing, sunspot_data):
 	precisions = [[x, y] for x in precisions_latitude for y in precisions_longitude]
 	
 	for index_not_disappearing, group_data_not_disappearing in data_not_disappearing.iterrows():
-		data_recurrent = sunspot_data.loc[((group_data_not_disappearing['StartJulianDay'] + 10) < sunspot_data['StartJulianDay']) &
-										  (sunspot_data['StartJulianDay'] < (group_data_not_disappearing['StartJulianDay'] + 30))]
+		data_recurrent = sunspot_data.loc[((group_data_not_disappearing['StartDay'] + 10) < sunspot_data['StartDay']) &
+										  (sunspot_data['StartDay'] < (group_data_not_disappearing['StartDay'] + 30))]
 					
 		# times matched
 		for index, group_data in data_recurrent.iterrows():
 			for precision_latitude, precision_longitude in precisions:			
-				if ((group_data_not_disappearing['MaxLatitude'] + precision_latitude) >= group_data['MinLatitude'] and 
-				(group_data_not_disappearing['MinLatitude'] + precision_latitude) <= group_data['MaxLatitude'] and 
-				(group_data_not_disappearing['MaxLongitude'] + precision_longitude) >= group_data['MinLongitude'] and 
-				(group_data_not_disappearing['MinLongitude'] + precision_longitude) <= group_data['MaxLongitude']) :
+				if ((group_data_not_disappearing['MaxLat'] + precision_latitude) >= group_data['MinLat'] and 
+				(group_data_not_disappearing['MinLat'] + precision_latitude) <= group_data['MaxLat'] and 
+				(group_data_not_disappearing['MaxLong'] + precision_longitude) >= group_data['MinLong'] and 
+				(group_data_not_disappearing['MinLong'] + precision_longitude) <= group_data['MaxLong']) :
 					# position matched
 					groups_matched.append([str(precision_longitude), str(group_data_not_disappearing['Year']), 
-										   str(group_data_not_disappearing['CarringtonRotation']), group_data_not_disappearing['NOAA'], 
+										   str(group_data_not_disappearing['CarrRot']), group_data_not_disappearing['NOAA'], 
 										   group_data['NOAA']])
 					break
 			
